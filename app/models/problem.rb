932299a1b79e0ba2b16a7f60736bead48ea696ad
Pavel Druziak <pavel.druzyak@gmail.com>
2015-07-18 19:03:11 +0300

    fix

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -14,6 +14,6 @@ class Problem < ActiveRecord::Base
   end
 
   def solution_for(participation)
-    solutions.find_by(participation: @participation)
+    solutions.find_by(participation: participation)
   end
 end
