45bff2516c5a84de1ba16a960cfb7b819d513aa6
Pavel Druziak <pavel.druzyak@gmail.com>
2015-07-18 19:38:37 +0300

    Edit and delete Solution

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -2,19 +2,42 @@ class SolutionsController < ApplicationController
   load_and_authorize_resource except: [:index, :new]
 
   def new
+    @solution = Solution.new problem_id: params[:problem_id],
+                             participation_id: params[:participation_id]
   end
 
   def create
     if @solution.save!
-      redirect_to @solution.problem.tour, notice: 'Saved'
+      redirect_to current_tour, notice: t('model.created')
     else
-      redirect_to @solution.problem.tour, error: 'Not Saved'
+      render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @solution.update(solution_params)
+      redirect_to current_tour, notice: t('model.updated')
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @solution.destroy
+
+    redirect_to current_tour, notice: t('model.destroyed')
   end
 
   private
 
   def solution_params
     params.require(:solution).permit(:content, :problem_id, :participation_id)
+  end
+
+  def current_tour
+    @solution.problem.tour
   end
 end
