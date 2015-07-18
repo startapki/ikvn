d7a4030b903855cb002173a8d0def38ca283c618
Yevhen Shemet <yevhene@gmail.com>
2015-07-18 18:15:19 +0300

    Styles

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -5,16 +5,16 @@ class SolutionsController < ApplicationController
   end
 
   def create
-  	if @solution.save!
-  	  redirect_to @solution.problem.tour, notice: 'Saved'
-  	else
-  	  redirect_to @solution.problem.tour, error: 'Not Saved'
-  	end
+    if @solution.save!
+      redirect_to @solution.problem.tour, notice: 'Saved'
+    else
+      redirect_to @solution.problem.tour, error: 'Not Saved'
+    end
   end
 
   private
 
   def solution_params
-  	params.require(:solution).permit(:content, :problem_id, :participation_id)
+    params.require(:solution).permit(:content, :problem_id, :participation_id)
   end
 end
