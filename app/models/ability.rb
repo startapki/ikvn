27cb66a61b0b25dec2f87ad78f838d4735534c13
Yevhen Shemet <yevhene@gmail.com>
2015-08-22 18:26:46 +0300

    Cop fixes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,24 +12,7 @@ class Ability
     can :create, Participation, user_id: user.id if user.persisted?
 
     if user.admin?
-      can :update, League
-      can :update, Tournament
-
-      can :manage, Season
-      cannot :destroy, Season, &:active?
-
-      can :manage, Tour
-      can :view_results, Tour
-      cannot :destroy, Tour, &:active?
-      cannot :judge, Tour
-
-      can :manage, Problem
-      cannot :destroy, Problem, &:solutions?
-
-      can :manage, Participation
-      can :read, Solution
-
-      can :manage, User
+      admin_can(user)
     else
       can :read, Season, Season.active, &:active?
       can :read, Tour, Tour.active, &:active?
@@ -39,15 +22,40 @@ class Ability
     end
 
     if user.judge?
-      can :read, Solution
-      can :manage, Score
-      can :judge, Tour do |tour|
-        tour.finished? && !tour.reviewed?
-      end
+      judge_can(user)
     elsif !user.admin? && user.persisted?
       can :manage, Solution do |solution|
         solution.problem.tour.solutionable?
       end
+    end
+  end
+
+  def admin_can(_user)
+    can :update, League
+    can :update, Tournament
+
+    can :manage, Season
+    cannot :destroy, Season, &:active?
+
+    can :manage, Tour
+    can :view_results, Tour
+    cannot :destroy, Tour, &:active?
+    cannot :judge, Tour
+
+    can :manage, Problem
+    cannot :destroy, Problem, &:solutions?
+
+    can :manage, Participation
+    can :read, Solution
+
+    can :manage, User
+  end
+
+  def judge_can(_user)
+    can :read, Solution
+    can :manage, Score
+    can :judge, Tour do |tour|
+      tour.finished? && !tour.reviewed?
     end
   end
 end
